6fea14f7b6edf7c8368cac03c0c84f6e7a51ad7f
elenapark <elenability@naver.com>
2023-04-05 00:54:12 +0900

    feat: service - 파일시스템으로 md파일을 읽어 리스트로 리턴해주는 getAllPosts함수 구현

---
+++ blogposts/nextjs-13.md
@@ -0,0 +1,7 @@
+---
+title:'Difference between NEXT12 and NEXT13'
+date:'2023-04-04'
+---
+
+**Hello**
+I'm going to talk about NextJS 13 version which is beta version **recently** released.
---
+++ src/service/posts.ts
@@ -0,0 +1,34 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+type BlogPost = {
+  id: string;
+  title: string;
+  date: string;
+};
+const postDir = path.join(process.cwd(), 'blogposts');
+
+export const getAllPosts = () => {
+  const postFileNames = fs.readdirSync(postDir);
+  return postFileNames.map((filename: string) => {
+    // id value를 얻기위한 .md 확장자명 제거
+    const id = filename.replace(/\.md$/, '');
+
+    // read md file as string
+
+    const filePath = path.join(postDir, filename);
+    const fileContents = fs.readFileSync(filePath, 'utf-8');
+
+    // post metadata를 가져오기 위한 gray-matter
+    const matterResult = matter(fileContents);
+
+    const blogPost: BlogPost = {
+      id,
+      title: matterResult.data.title,
+      date: matterResult.data.date,
+    };
+
+    return blogPost;
+  });
+};
